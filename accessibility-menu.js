f866475f20cfc8408ec433a9249a24314477cd4f
praboth charith - GBS - CLI <probothcharith@gmail.com>
2024-04-24 08:55:19 +0530

    unwanted function removed

--- accessibility-menu.js
+++ accessibility-menu.js
@@ -919,9 +919,6 @@ document.addEventListener("DOMContentLoaded", function () {
             '</svg>' : accessibilityModal.classList.contains('right') ? '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">\n' +
             '  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>\n' +
             '</svg>' : '';
-        accessibilityTools.style.display = accessibilityModal.classList.contains('close') ? setTimeout(() => {
-            accessibilityTools.style.display = 'none';
-        }, 200) : 'flex';
     });
 
     const accItems = document.querySelectorAll('.acc-item');
